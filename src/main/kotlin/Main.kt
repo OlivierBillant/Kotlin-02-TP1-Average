646e0e637209e59d853999a4a6e47524fb752f53
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-05 10:30:50 +0200

    ADD toutes fonctions moyenne fonctionnelles

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,43 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+//    calculMoyenneFixe()
+    calculMoyenneVariable()
+//    println("Program arguments: ${args.joinToString()}")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+}
+
+fun calculMoyenneFixe() {
+    println("Entrez le nombre de valeurs :")
+    var nombreValeurs = readLine()!!.toInt()
+    var listeNotes = arrayListOf<Int>()
+    var totalNotes = 0
+    println("Vous souhaitez calculer la moyenne de $nombreValeurs notes")
+
+
+    for (i in 1..nombreValeurs) {
+        println("Valeur :")
+        listeNotes.add(readLine()!!.toInt())
+    }
+
+    for (note in listeNotes) {
+        totalNotes += note
+    }
+    println("La moyenne est de " + totalNotes / (nombreValeurs) + "/20")
+}
+
+fun calculMoyenneVariable() {
+    var listeNotes = arrayListOf<Int>()
+    var totalNotes = 0
+    println("Calculez la moyenne des notes suivantes")
+    do {
+        println("Note (-1 pour terminer) : ")
+        listeNotes.add(readLine()!!.toInt())
+    } while (listeNotes.last() != -1)
+    listeNotes.removeLast()
+    for (note in listeNotes) {
+        totalNotes += note
+    }
+    println("La moyenne est de " + totalNotes / (listeNotes.size) + "/20")
+
 
 }
